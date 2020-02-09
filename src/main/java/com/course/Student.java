62aad0bcfd2cb2a1e081cf84c48214ec005d3a66
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-02-09 16:57:39 +0100

    Student class implemented

--- src/main/java/com/course/Student.java
+++ src/main/java/com/course/Student.java
@@ -1,4 +1,26 @@
 package com.course;
 
 public class Student {
+    private static int nextId = 0;
+    private final int id;
+    private String firstName,lastName;
+
+    public Student(String firstName,String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = nextId;
+        nextId++;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public void registerForCourse(Course course){
+        course.registerStudent(this);
+    }
+    public void unregisterForCourse(Course course){
+        course.unregisterStudent(this);
+    }
 }
